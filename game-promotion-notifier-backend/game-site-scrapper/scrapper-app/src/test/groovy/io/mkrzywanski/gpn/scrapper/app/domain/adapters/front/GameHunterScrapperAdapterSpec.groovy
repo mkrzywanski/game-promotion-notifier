42c71b9c6c778f4de324152461b56c988e10f3b6
Michal Krzywanski <outcast1231@wp.pl>
2021-06-12 16:27:32 +0200

    fixing issue with mongodb connection during tests

--- game-promotion-notifier-backend/game-site-scrapper/scrapper-app/src/test/groovy/io/mkrzywanski/gpn/scrapper/app/domain/adapters/front/GameHunterScrapperAdapterSpec.groovy
+++ game-promotion-notifier-backend/game-site-scrapper/scrapper-app/src/test/groovy/io/mkrzywanski/gpn/scrapper/app/domain/adapters/front/GameHunterScrapperAdapterSpec.groovy
@@ -1,20 +1,19 @@
 package io.mkrzywanski.gpn.scrapper.app.domain.adapters.front
 
-import io.mkrzywanski.gpn.scrapper.app.GameSiteScrapperApp
 
 import io.mkrzywanski.gpn.scrapper.domain.gamehunter.GameHunterScrapperService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Primary
+import org.springframework.scheduling.annotation.EnableScheduling
 import spock.lang.Specification
 
 import java.time.Duration
 
 import static org.mockito.Mockito.*
 
-@SpringBootTest(properties = ["gpn.scheduling.cron=*/1 * * * * *"], classes = [GameSiteScrapperApp])
+@SpringBootTest(properties = ["gpn.scheduling.cron=*/1 * * * * *"])
 class GameHunterScrapperAdapterSpec extends Specification {
 
     private static final Duration TWO_SECONDS = Duration.ofSeconds(2)
@@ -34,11 +33,16 @@ class GameHunterScrapperAdapterSpec extends Specification {
     }
 
     @Configuration
+    @EnableScheduling
     static class TestConfig {
         @Bean
-        @Primary
         GameHunterScrapperService gameHunterScrapperServiceMock() {
             mock(GameHunterScrapperService)
+        }
+
+        @Bean
+        GameHunterScrapperAdapter gameHunterScrapperAdapter(GameHunterScrapperService gameHunterScrapperService) {
+            new GameHunterScrapperAdapter(gameHunterScrapperService)
         }
     }
 }

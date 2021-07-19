6cea5374009af437887de11c00fd039f6551a2d4
Michal Krzywanski <outcast1231@wp.pl>
2021-07-19 20:26:24 +0200

    test fixes

--- game-promotion-notifier-backend/user-service/user-app/src/integTest/groovy/io/mkrzywanski/gpn/user/app/TestDatabaseProperties.groovy
+++ game-promotion-notifier-backend/user-service/user-app/src/integTest/groovy/io/mkrzywanski/gpn/user/app/TestDatabaseProperties.groovy
@@ -1,18 +1,43 @@
 package io.mkrzywanski.gpn.user.app
 
-import lombok.AllArgsConstructor
-import lombok.Getter
+
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
 
 @ConstructorBinding
 @ConfigurationProperties("database")
-@AllArgsConstructor
-@Getter
 class TestDatabaseProperties {
     private String driver
     private String host
     private String username
     private String password
     private String database
+
+    TestDatabaseProperties(String driver, String host, String username, String password, String database) {
+        this.driver = driver
+        this.host = host
+        this.username = username
+        this.password = password
+        this.database = database
+    }
+
+    String getDriver() {
+        return driver
+    }
+
+    String getHost() {
+        return host
+    }
+
+    String getUsername() {
+        return username
+    }
+
+    String getPassword() {
+        return password
+    }
+
+    String getDatabase() {
+        return database
+    }
 }

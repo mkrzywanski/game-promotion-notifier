1bd8969fec7e58ddd4167a3fadac30f77895ea81
Michal Krzywanski <outcast1231@wp.pl>
2021-07-24 17:18:11 +0200

    fix

--- promotion-notifier-backend/pn-user-service/user-app/src/main/java/io/mkrzywanski/pn/user/app/infra/Config.java
+++ promotion-notifier-backend/pn-user-service/user-app/src/main/java/io/mkrzywanski/pn/user/app/infra/Config.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
-@EnableJpaRepositories(basePackages = {"io.mkrzywanski.gpn.user.app"})
+@EnableJpaRepositories(basePackages = {"io.mkrzywanski.pn.user.app"})
 class Config {
 
     @Bean

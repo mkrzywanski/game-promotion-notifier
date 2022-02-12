a5fe15b42c6dea9e4c7e433cea3c2f2ff72c9317
mkrzywanski <michal.krzywanski1@gmail.com>
2022-02-12 13:35:55 +0100

    removed comments

--- k8s/setup-all.sh
+++ k8s/setup-all.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 set -euxo pipefail
+
 ./setup-helm-repositories.sh
 ./setup-operators.sh
 helm dependency update backend-app 
 
 ./upload-fresh-images.sh
-
-# helm install mongodb bitnami/mongodb -f ./mongodb/values.yml --version 11.0.0
-
-# helm install mongodb-matching-service bitnami/mongodb -f ./mongodb/values.yml --version 11.0.0
-
-# helm install mongodb-scrapping-service bitnami/mongodb -f ./mongodb/values.yml --version 11.0.0
-
-# helm install elasticsearch elastic/elasticsearch -f ./elasticsearch/values.yml --version 7.16.3
 
 ./run-helm-charts.sh

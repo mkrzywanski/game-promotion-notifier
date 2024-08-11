10827f3d943bf1d77effd112c51e93087597dd31
mkrzywanski <michal.krzywanski1@gmail.com>
2024-08-11 13:02:55 +0200

    fix

--- promotion-notifier-backend/pn-matching-service/src/main/java/io/mkrzywanski/pn/matching/infra/queue/PostsQueueConfig.java
+++ promotion-notifier-backend/pn-matching-service/src/main/java/io/mkrzywanski/pn/matching/infra/queue/PostsQueueConfig.java
@@ -4,7 +4,6 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.amqp.RabbitTemplateCustomizer;
 import org.springframework.context.annotation.Bean;
